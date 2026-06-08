977c17cb34fd4902e3e214a75c8ded09d63e8e92
BusinessPros <programmieren.at@gmail.com>
2026-06-08 08:43:40 +0200

    Prepare shared repository workflow

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,9 +9,15 @@ const eslintConfig = defineConfig([
   globalIgnores([
     // Default ignores of eslint-config-next:
     ".next/**",
+    "**/.next/**",
     "out/**",
     "build/**",
     "next-env.d.ts",
+    "node_modules/**",
+    ".idea/**",
+    "webvault/**",
+    "webvault-qa/**",
+    "devserver.*.log",
   ]),
 ]);
 
